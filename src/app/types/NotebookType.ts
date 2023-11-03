a68038146436027dfd8bcea1391fae2ae394cc57
C410lol <gcaio7463@gmail.com>
2023-11-02 21:12:57 -0300

    Modifying fields in types

---
+++ src/app/types/NotebookType.ts
@@ -0,0 +1,13 @@
+export type NotebookType = {
+
+    id?: number,
+    classe: string,
+    subject: string,
+    bimester: string,
+    status?: string,
+    createDate?: string,
+    students?: number,
+    lessons?: number,
+    works?: number,
+
+}
---
+++ src/app/types/WorkType.ts
@@ -0,0 +1,11 @@
+export type WorkType = {
+
+    id?: number,
+    title: string,
+    details: string,
+    observations: string,
+    type: string,
+    deliveryDate: string
+    grades?: number
+
+}
