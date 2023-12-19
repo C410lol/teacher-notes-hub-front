b2832c7e74c442b0121968e8d3e4ef1f4b8446d2
C410lol <gcaio7463@gmail.com>
2023-12-19 13:24:11 -0300

    Extracting 'login' and 'create' routes/components to inside the main component 'pages-component'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { CreatePageComponent } from './pages/create-page/create-page.component';
 import { PagesRoutingModule } from './pages/pages-routing.module';
 
 
 const routes: Routes = [
-  {
-    path: 'login', component: LoginPageComponent
-  },
-  {
-    path: 'create', component: CreatePageComponent
-  },
   {
     path: "", component: PagesComponent, loadChildren: () => PagesRoutingModule
   },
