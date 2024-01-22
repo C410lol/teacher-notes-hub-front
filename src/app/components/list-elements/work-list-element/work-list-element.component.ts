c5354b6a618e51cfdbb7a160d038e19a094182e3
C410lol <gcaio7463@gmail.com>
2024-01-22 14:13:22 -0300

    Adding 'type' column in works page

--- src/app/components/list-elements/work-list-element/work-list-element.component.ts
+++ src/app/components/list-elements/work-list-element/work-list-element.component.ts
@@ -11,6 +11,7 @@ export class WorkListElementComponent {
   @Input() id?: number = 0;
   @Input() date: string = '';
   @Input() title: string = '';
+  @Input() type: string = '';
   @Input() grades?: number = 0;
   @Input() students: number = 0;
 
