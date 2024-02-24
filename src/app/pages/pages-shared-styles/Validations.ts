d984454f9540c8c8d4a9966c5b853b6847fefb55
C410lol <gcaio7463@gmail.com>
2024-02-24 13:25:32 -0300

    Creating 'validations' class

---
+++ src/app/pages/pages-shared-styles/Validations.ts
@@ -0,0 +1,12 @@
+export const Validations = {
+
+    isNotBlank: (values: string[]): boolean => {
+        if (values.length < 1) return false;
+        for (let x: number = 0; x < values.length; x++) {
+            if (values[x] == null) return false;
+            if (values[x].trim().length < 1) return false;
+        }
+        return true;
+    }
+
+}
