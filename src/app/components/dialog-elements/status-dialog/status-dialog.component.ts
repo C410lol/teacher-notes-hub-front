c5a770607a11700f5198929cd63162e075bc7a0b
C410lol <gcaio7463@gmail.com>
2024-02-24 13:22:08 -0300

    Creating 'status-dialog' component

---
+++ src/app/components/dialog-elements/status-dialog/status-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-status-dialog',
+  templateUrl: './status-dialog.component.html',
+  styleUrls: ['./status-dialog.component.css']
+})
+export class StatusDialogComponent {
+
+  @Output() destroyButtonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  @Input() type: string = '';
+  @Input() title: string = 'Carregando...';
+  @Input() content: string = 'Carregando...';
+
+  constructor() { }
+
+  destroyComponent(): void {
+    this.destroyButtonClick.emit();
+  }
+
+}
